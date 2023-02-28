1656cb27cd1d6df4733ba8eda0f060270465baa4
Nguyen (Jenny) Hoang <nguyen_hoang@m018906.aw.org>
2023-02-27 16:20:46 -0800

    haiku updated

--- Haiku.py
+++ Haiku.py
@@ -7,12 +7,18 @@ def genLine(syl, val, exclude, include):
 # use indefinite loop because there are no limited words
     while True:
         v = random.randint(1,4)
+# val is the number of syllables in one line
         if val - v >= 0:
+# get the list of syllables, get(v) is get the key
             myList = syl.get(v).split()
+# choose a random word from the list
             word = random.choice(myList)
+# if word is in the letter that user want to exclude, then use words that do not have that letter
+# if word is in the letter that user want to include, then use words that have that letter
             if exclude not in word and include in word:
                 line = line + word + " "
                 val = val - v
+#if value is 0, break the loop
         if val == 0:
             break
     
@@ -38,6 +44,7 @@ def main():
     mulHaiku = int(input("How many Haiku do you want to make?\n"))
     include = input("What letter do you want to include in the Haiku?\n")
     exclude = input("What letter do you want to exclude in the Haiku?\n")
+# the number of Haiku is the number that user wants
     for i in range(mulHaiku):
         print(genLine(syl, 5, exclude, include))
         print(genLine(syl, 7, exclude, include))
